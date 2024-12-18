14c4e872371501241541ad5848d9a2dc5a6bf1b4
DanRussuleac <drussuleac@gmail.com>
2024-12-18 18:40:42 +0000

    t

--- mindcare-frontend/src/index.js
+++ mindcare-frontend/src/index.js
@@ -1,9 +1,8 @@
-// index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { ThemeProvider } from '@mui/material/styles';
-import theme from './theme'; // Custom theme file
+import theme from './theme'; 
 import CssBaseline from '@mui/material/CssBaseline';
 
 ReactDOM.render(
